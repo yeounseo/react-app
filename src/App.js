629f5a498784bf99f21a88401113be8cad3dc531
yeounseo <yeoandante@gmail.com>
2019-04-29 23:24:36 +0900

    Test
    Test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    // 초기화를 담당하는 constructor
+    super(props);
+    this.state = {
+      mode: 'read',
+      subject:{title: 'WEB', sub:'World Wide Web!'},
+      welcome:{title: 'Welcome', desc:'Hello, React!!'},
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
+    }
+  }
+  render() {
+    console.log('App render');
+    
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    return (
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub} 
+        onChangePage={function(){
+          this.setState({
+            mode:'welcome'
+          });
+        }.bind(this)}
+        />
+        <TOC data={this.state.contents}/>
+        <Content title={_title} desc={_desc}/>
+      </div>
+    );
+  }
 }
 
 export default App;

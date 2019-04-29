34ce9f6da9305def5d8188d20cdf13aafc3372dd
yeounseo <yeoandante@gmail.com>
2019-04-29 23:50:44 +0900

    add function
    add onChagePage function

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode: 'read',
+      selected_content_id:2,
       subject:{title: 'WEB', sub:'World Wide Web!'},
       welcome:{title: 'Welcome', desc:'Hello, React!!'},
       contents: [
@@ -27,8 +28,16 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
@@ -39,7 +48,15 @@ class App extends Component {
           });
         }.bind(this)}
         />
-        <TOC data={this.state.contents}/>
+        <TOC 
+          onChangePage={function(id){
+            
+            this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          });
+        }.bind(this)}
+        data={this.state.contents} />
         <Content title={_title} desc={_desc}/>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -8,7 +8,18 @@ class TOC extends Component{
       var data = this.props.data;
       var i = 0;
       while(i < data.length){
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/"+data[i].id}
+            // data-id={data[i].id}
+            onClick={function(id, e){
+              e.preventDefault();
+              // this.props.onChangePage(e.target.dataset.id);
+              this.props.onChangePage(id);
+            }.bind(this, data[i].id)}
+          >{data[i].title}</a>
+        </li>);
         i = i + 1;
       }
       return (
